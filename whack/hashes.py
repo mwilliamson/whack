3c5233a620a9c737c7bffb479631329998c4cffa
Michael Williamson <mike@zwobble.org>
2012-10-24 21:48:14 +0100

    Move hashing functions into separate class

---
+++ whack/hashes.py
@@ -0,0 +1,33 @@
+import os
+import hashlib
+
+class Hasher(object):
+    def __init__(self):
+        self._hash = hashlib.sha1()
+    
+    def update(self, arg):
+        self._hash.update(_sha1(arg))
+    
+    def update_with_dir(dir_path)
+        for file_path in _all_files(dir_path):
+            self._update_with_file(file_path)
+    
+    def _update_with_file(self, path):
+        self.update(path)
+        self.update(open(path).read())
+        
+    
+    def hexdigest(self):
+        return self._hash.hexdigest()
+
+def _all_files(top):
+    all_files = []
+    
+    for root, dirs, files in os.walk(top):
+        for name in files:
+            all_files.append(os.path.join(root, name))
+    
+    return all_files
+
+def _sha1(str):
+    return hashlib.sha1(str).hexdigest()
