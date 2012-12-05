b59f09f02c3f22ba53f08790babd75348153d64b
Michael Williamson <mike@zwobble.org>
2012-12-05 22:30:49 +0000

    Add test for hashing multiple values

--- tests/hashes_test.py
+++ tests/hashes_test.py
@@ -10,3 +10,13 @@ def hashing_the_same_single_value_gives_the_same_hash():
         return hasher.hexdigest()
     
     assert_equal(create_hash(), create_hash())
+
+@istest
+def hashing_multiple_values_in_the_same_order_gives_the_same_hash():
+    def create_hash():
+        hasher = Hasher()
+        hasher.update("one")
+        hasher.update("two")
+        return hasher.hexdigest()
+    
+    assert_equal(create_hash(), create_hash())
