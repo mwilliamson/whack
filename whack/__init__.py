e705014dc7c81d3668fa3fefa510f9131cce199a
Michael Williamson <mike@zwobble.org>
2012-12-07 21:55:50 +0000

    Revert "Move main function into the main method of a class"
    This reverts commit d5e5e6d16d92d210136e0242112a88231f1e5c05.

--- whack/__init__.py
+++
@@ -1,6 +0,0 @@
-import whack.builder
-
-class WhackOperations(object):
-    def install(self, package, install_dir, should_cache, builder_uris):
-        builder = whack.builder.Builders(should_cache, builder_uris)
-        builder.build_and_install(package, install_dir)
