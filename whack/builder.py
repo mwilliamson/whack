b1cf189bb7b9cda047090fce6bf23c46bb48e549
Michael Williamson <mike@zwobble.org>
2012-12-09 16:46:36 +0000

    Fetch packages specified by source control URIs as archives to cache builds

--- whack/builder.py
+++ whack/builder.py
@@ -1,4 +1,7 @@
 import os
+import contextlib
+import tempfile
+import shutil
 
 import blah
 
@@ -11,10 +14,10 @@ class Builders(object):
         self._builder_repo_urls = builder_repo_uris
 
     def build_and_install(self, package_name, install_dir, params=None):
-        scripts_dir = self._fetch_package(package_name)
-        builder = PackageInstaller(scripts_dir, self._create_cacher())
-        return builder.install(install_dir, params=params)
-
+        with self._fetch_package(package_name) as package_dir:
+            builder = PackageInstaller(package_dir, self._create_cacher())
+            return builder.install(install_dir, params=params)
+    
     def _fetch_package(self, package):
         if blah.is_source_control_uri(package):
             package_dir = self._fetch_package_from_source_control(package)
@@ -26,9 +29,14 @@ class Builders(object):
         else:
             return package_dir
     
+    @contextlib.contextmanager
     def _fetch_package_from_source_control(self, package):
-        return downloads.fetch_source_control_uri(package)
+        with _create_temporary_dir() as temporary_dir:
+            archive_dir = os.path.join(temporary_dir, "archive")
+            blah.fetch(package, archive_dir, archive=True)
+            yield archive_dir
     
+    @contextlib.contextmanager
     def _fetch_package_from_repo(self, package):
         for uri in self._builder_repo_urls:
             if self._is_local_uri(uri):
@@ -38,7 +46,7 @@ class Builders(object):
             # FIXME: race condition between this and when we acquire the lock
             package_dir = os.path.join(repo_dir, package)
             if os.path.exists(package_dir):
-                return package_dir
+                yield package_dir
 
     def _create_cacher(self):
         if self._should_cache:
@@ -49,5 +57,10 @@ class Builders(object):
     def _is_local_uri(self, uri):
         return "://" not in uri
 
-
-
+@contextlib.contextmanager
+def _create_temporary_dir():
+    try:
+        build_dir = tempfile.mkdtemp()
+        yield build_dir
+    finally:
+        shutil.rmtree(build_dir)
