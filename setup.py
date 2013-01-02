23f48cd728156718d871cf1b002d8cd2a75e2446
Michael Williamson <mike@zwobble.org>
2013-01-02 22:47:06 +0000

    Depend on catchy-0.1.2 so that builds with symlinks work correctly

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ def read(fname):
 
 setup(
     name='whack',
-    version='0.3.1',
+    version='0.3.2',
     description='Utility for installing binaries from source with a single command',
     long_description=read("README"),
     author='Michael Williamson',
     url='http://github.com/mwilliamson/whack',
     scripts=["scripts/whack"],
     packages=['whack'],
-    install_requires=['blah>=0.1.10,<0.2', 'requests>=1,<2', "catchy>=0.1.0,<0.2"],
+    install_requires=['blah>=0.1.10,<0.2', 'requests>=1,<2', "catchy>=0.1.2,<0.2"],
 )
