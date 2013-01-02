3e0ea05d8c1ff44f7733758d1f053d31456d5a24
Michael Williamson <mike@zwobble.org>
2013-01-02 12:10:29 +0000

    Create separate working dir and install dir for non-relocatable builds

--- whack/installer.py
+++ whack/installer.py
@@ -21,44 +21,48 @@ class PackageInstaller(object):
         
         with create_temporary_dir() as temp_dir:
             build_dir = os.path.join(temp_dir, "build")
-            result = self._cacher.fetch(install_id, build_dir)
+            working_dir = os.path.join(build_dir, "workspace")
+            cached_install_dir = os.path.join(build_dir, "install")
+            
+            result = self._cacher.fetch(install_id, working_dir)
             if not result.cache_hit:
-                self._build(build_dir, params)
-                self._cacher.put(install_id, build_dir)
+                self._build(working_dir, cached_install_dir, params)
+                self._cacher.put(install_id, working_dir)
             
             if self._is_relocatable():
-                self._run_install_script(build_dir, install_dir)
+                self._run_install_script(working_dir, install_dir)
             else:
                 # TODO: should be pure Python, but there isn't a stdlib function
                 # that allows the destination to already exist
-                subprocess.check_call(["cp", "-rT", build_dir, install_dir])
+                subprocess.check_call(["cp", "-rT", cached_install_dir, install_dir])
                 with open(os.path.join(install_dir, "run"), "w") as run_file:
                     run_file.write('#!/usr/bin/env sh\nexec whack-run-with-whack-root \'{0}\' "$@"'.format(install_dir))
                 subprocess.check_call(["chmod", "+x", os.path.join(install_dir, "run")])
 
-    def _build(self, build_dir, params):
+    def _build(self, working_dir, install_dir, params):
         ignore = shutil.ignore_patterns(".svn", ".hg", ".hgignore", ".git", ".gitignore")
-        shutil.copytree(self._package_dir, build_dir, ignore=ignore)
+        shutil.copytree(self._package_dir, working_dir, ignore=ignore)
         build_env = params_to_build_env(params)
-        self._fetch_downloads(build_dir, build_env)
+        self._fetch_downloads(working_dir, build_env)
         build_script = os.path.join(self._package_dir, "whack/build")
         if self._is_relocatable():
             build_command = [build_script]
         else:
-            build_command = ["whack-run-with-whack-root", build_dir, build_script, _WHACK_ROOT]
+            os.mkdir(install_dir)
+            build_command = ["whack-run-with-whack-root", install_dir, build_script, _WHACK_ROOT]
             
         subprocess.check_call(
             build_command,
-            cwd=build_dir,
+            cwd=working_dir,
             env=build_env
         )
         
-        def build_path(path):
-            return os.path.join(build_dir, path)
+        def install_path(path):
+            return os.path.join(install_dir, path)
         
         if not self._is_relocatable():
-            dot_bin_dir = build_path(".bin")
-            bin_dir = build_path("bin")
+            dot_bin_dir = install_path(".bin")
+            bin_dir = install_path("bin")
             if os.path.exists(dot_bin_dir) and not os.path.exists(bin_dir):
                 os.mkdir(bin_dir)
                 for bin_filename in os.listdir(dot_bin_dir):
