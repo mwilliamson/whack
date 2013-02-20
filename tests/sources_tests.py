9d05daee638d87cff742933721ce5bb9f3835abf
Michael Williamson <mike@zwobble.org>
2013-02-20 19:22:25 +0000

    Remove duplication in tests

--- tests/sources_tests.py
+++ tests/sources_tests.py
@@ -11,40 +11,40 @@ from whack.files import read_file, write_file, write_files, plain_file
 
 @istest
 def can_fetch_package_source_from_source_control():
-    with create_temporary_dir() as package_source_dir:
-        write_files(package_source_dir, [
-            plain_file("whack/whack.json", json.dumps({})),
-            plain_file("whack/name", "Bob"),
-        ])
+    def put_package_source_into_source_control(package_source_dir):
         _convert_to_git_repo(package_source_dir)
+        return "git+file://{0}".format(package_source_dir)
         
-        source_fetcher = PackageSourceFetcher()
-        repo_uri = "git+file://{0}".format(package_source_dir)
-        with source_fetcher.fetch(repo_uri) as package_source:
-            with create_temporary_dir() as target_dir:
-                package_source.write_to(target_dir)
-                assert_equal(
-                    "Bob",
-                    read_file(os.path.join(target_dir, "whack/name"))
-                )
+    _assert_package_source_can_be_written_to_target_dir(
+        put_package_source_into_source_control
+    )
 
 
 @istest
 def can_fetch_package_source_from_local_path():
+    _assert_package_source_can_be_written_to_target_dir(
+        lambda package_source_dir: package_source_dir
+    )
+
+
+def _assert_package_source_can_be_written_to_target_dir(source_filter):
     with create_temporary_dir() as package_source_dir:
         write_files(package_source_dir, [
             plain_file("whack/whack.json", json.dumps({})),
             plain_file("whack/name", "Bob"),
         ])
         
+        package_source_name = source_filter(package_source_dir)
+        
         source_fetcher = PackageSourceFetcher()
-        with source_fetcher.fetch(package_source_dir) as package_source:
+        with source_fetcher.fetch(package_source_name) as package_source:
             with create_temporary_dir() as target_dir:
                 package_source.write_to(target_dir)
                 assert_equal(
                     "Bob",
                     read_file(os.path.join(target_dir, "whack/name"))
                 )
+    
 
 
 @istest
