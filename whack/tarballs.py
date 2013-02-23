3ea2275d1df86eaf8c59ad4033b49ad9a87ce1bd
Michael Williamson <mike@zwobble.org>
2013-02-23 20:20:44 +0000

    Fix indentation in extract_tarball

--- whack/tarballs.py
+++ whack/tarballs.py
@@ -5,12 +5,12 @@ from .files import mkdir_p
 
 
 def extract_tarball(tarball_path, destination_dir, strip_components):
-        mkdir_p(destination_dir)
-        subprocess.check_call([
-            "tar", "xzf", tarball_path,
-            "--directory", destination_dir,
-            "--strip-components", str(strip_components)
-        ])
+    mkdir_p(destination_dir)
+    subprocess.check_call([
+        "tar", "xzf", tarball_path,
+        "--directory", destination_dir,
+        "--strip-components", str(strip_components)
+    ])
 
 
 def create_tarball(tarball_path, source):
