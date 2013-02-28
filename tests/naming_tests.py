6ccd4e95af85c9008927419bad3175896cb7de2d
Michael Williamson <mike@zwobble.org>
2013-02-28 21:04:48 +0000

    Pass description dict into fake package source in tests

--- tests/naming_tests.py
+++ tests/naming_tests.py
@@ -1,39 +1,42 @@
 from nose.tools import istest, assert_equal
 
 from whack.naming import PackageNamer
+from whack.sources import DictBackedPackageDescription
 
 
 @istest
 def package_with_unnamed_source_has_name_equal_to_install_identifier():
-    package_source = PackageSource("/tmp/nginx-src", None, None)
+    package_source = PackageSource("/tmp/nginx-src", {})
     package_name = _name_package(package_source, {})
     assert_equal("install-id(/tmp/nginx-src, {})", package_name)
 
 
 @istest
 def params_are_passed_to_install_id_generator():
-    package_source = PackageSource("/tmp/nginx-src", None, None)
+    package_source = PackageSource("/tmp/nginx-src", {})
     package_name = _name_package(package_source, {"version": "1.2"})
     assert_equal("install-id(/tmp/nginx-src, {'version': '1.2'})", package_name)
     
 
 @istest
 def package_with_named_source_has_name_equal_to_name_of_source_followed_by_install_identifier():
-    package_source = PackageSource("/tmp/nginx-src", "nginx", None)
+    package_source = PackageSource("/tmp/nginx-src", {"name": "nginx"})
     package_name = _name_package(package_source, {})
     assert_equal("nginx-install-id(/tmp/nginx-src, {})", package_name)
     
 
 @istest
 def param_slug_is_included_if_present_in_description():
-    package_source = PackageSource("/tmp/nginx-src", "nginx", "1.2")
+    description = {"name": "nginx", "paramSlug": "1.2"}
+    package_source = PackageSource("/tmp/nginx-src", description)
     package_name = _name_package(package_source, {})
     assert_equal("nginx-1.2-install-id(/tmp/nginx-src, {})", package_name)
 
 
 @istest
 def format_string_is_expanded_in_param_slug():
-    package_source = PackageSource("/tmp/nginx-src", "nginx", "{nginx_version}")
+    description = {"name": "nginx", "paramSlug": "{nginx_version}"}
+    package_source = PackageSource("/tmp/nginx-src", description)
     package_name = _name_package(package_source, {"nginx_version": "1.2"})
     _assert_startswith(package_name, "nginx-1.2-install-id")
 
@@ -53,16 +56,15 @@ def _assert_startswith(string, prefix):
 
 
 class PackageSource(object):
-    def __init__(self, path, name, param_slug):
+    def __init__(self, path, description):
         self.path = path
-        self._name = name
-        self._param_slug = param_slug
+        self._description = description
         
     def name(self):
-        return self._name
+        return self.description().name()
         
     def description(self):
-        return Description(self._param_slug)
+        return DictBackedPackageDescription(self._description)
 
 
 class Description(object):
