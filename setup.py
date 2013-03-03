e1386c3d3fb7e45d5f8557e5d4996867d214f646
Michael Williamson <mike@zwobble.org>
2013-03-03 00:29:25 +0000

    Update to catchy 0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         'blah>=0.1.10,<0.2',
         'requests>=1,<2',
-        "catchy>=0.1.3,<0.2",
+        "catchy>=0.2.0,<0.3",
         "locket>=0.1.1,<0.2",
         "beautifulsoup4>=4.1.3,<5",
     ],
