47aaf00f2f76d0b6942f082d2bdf66b4130eec87
Michael Williamson <mike@zwobble.org>
2013-04-20 13:16:10 +0100

    Add test for Downloader.download

--- tests/downloads_test.py
+++ tests/downloads_test.py
@@ -1,6 +1,12 @@
-from nose.tools import istest, assert_equal
+import os
 
-from whack.downloads import read_downloads_string, Download
+from nose.tools import istest, assert_equal
+from catchy import NoCachingStrategy
+
+from whack.downloads import read_downloads_string, Download, Downloader
+from whack import files
+from whack.tempdir import create_temporary_dir
+from . import httpserver
 
 
 @istest
@@ -29,3 +35,17 @@ def filename_can_be_explicitly_set_after_url():
         [Download("http://nginx.org/download/nginx-1.2.6.tar.gz", "nginx.tar.gz")],
         read_downloads_string("http://nginx.org/download/nginx-1.2.6.tar.gz nginx.tar.gz")
     )
+
+
+@istest
+def downloader_can_download_files_over_http():
+    downloader = Downloader(NoCachingStrategy())
+    
+    with create_temporary_dir() as server_root:
+        files.write_file(os.path.join(server_root, "hello"), "Hello there!")
+        with httpserver.start_static_http_server(server_root) as http_server:
+            with create_temporary_dir() as download_dir:
+                download_path = os.path.join(download_dir, "file")
+                url = http_server.static_url("hello")
+                downloader.download(url, download_path)
+                assert_equal("Hello there!", files.read_file(download_path))
