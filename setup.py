b692e57988b279c8d2c4f88bf3995bc7b8bde355
Michael Williamson <mike@zwobble.org>
2013-10-12 12:41:14 +0100

    Update dodge.py for better support for versions of Python other than 2.7

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "catchy>=0.2.0,<0.3",
         "beautifulsoup4>=4.1.3,<5",
         "spur.local>=0.3.6,<0.4",
-        "dodge>=0.1.4,<0.2",
+        "dodge>=0.1.5,<0.2",
         "six>=1.4.1,<2.0"
     ],
     **extra
